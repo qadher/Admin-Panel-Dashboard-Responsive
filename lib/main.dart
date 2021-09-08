84a3da98e3f409151e3db9448251b4a60d4a3b0c
qadher <qadher7@gmail.com>
2021-09-08 14:28:59 +0530

    project started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:admin_panel_dashboard/homepage.dart';
+import 'package:admin_panel_dashboard/constants.dart';
+import 'package:admin_panel_dashboard/screens/main/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
       ),
-      home: HomePage(),
+      home: MainScreen(),
     );
   }
 }
-
